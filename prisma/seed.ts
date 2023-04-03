b46875bdad4fee3cd0c3355074080f101c5788be
xlincw0w <khaled.khazem@yahoo.com>
2023-04-03 18:14:50 +0100

    Docker, Optimization and Routing loader

---
+++ prisma/seed.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from "@prisma/client";
+// import { hash } from "@webapp/helpers/encryption/encrypt";
+import { hash } from "../src/helpers/encryption/encrypt";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  console.log("Seeding");
+  await prisma.admin.create({
+    data: {
+      name: process.env.ADMIN_NAME as string,
+      email: process.env.ADMIN_EMAIL as string,
+      password: await hash(process.env.ADMIN_PASSWORD as string),
+    },
+  });
+}
+
+main()
+  .catch((e) => {
+    console.log(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
